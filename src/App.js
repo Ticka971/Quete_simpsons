269f8962c04af4776bebef6f645afc2ff56d87f2
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-10-07 16:36:28 +0200

    creation de card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './components/NavBar'
-
+import QuoteCard from "./components/QuoteCard"
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <QuoteCard/>
     </div>
   );
 }
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Quotecard.css"
+
+function QuoteCard() {
+    return (
+        <div className="QuoteCard">
+            <figure class="QuoteCard">
+                <img
+                    src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+                    alt="Nelson Muntz" />
+                <figcaption>
+                    <blockquote>
+                        Shoplifting is a victimless crime, like punching someone in the dark.
+                    </blockquote>
+                    <cite>Nelson Muntz</cite>
+                </figcaption>
+            </figure>
+        </div>
+    );
+}
+
+export default QuoteCard;
+
