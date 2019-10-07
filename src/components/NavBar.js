3f9bd1e402cdb7c53dfad70fe21d0f08b8a92e51
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-10-07 15:41:04 +0200

    Création d'une barre de navigation

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Navbar from "./Navbar.css"
+
+function NavBar () {
+    return (
+        <nav className = "Navbar"> <a href ="https://fr.wikipedia.org/wiki/Les_Simpson" > Les simpsons</a></nav>
+    )
+}
+
+export default NavBar;
